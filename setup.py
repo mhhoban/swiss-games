37820ab32862e06bf23188193d3dc10b094003bb
Morgan Hoban <mhhoban@gmail.com>
2017-01-29 01:12:11 -0800

    setup.py fix

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ import subprocess
 
 print "Setting up environment for Swiss Games:"
 subprocess.call("pip install psycopg2")
-subprocess.call("createdb tournament", shell=True)
-subprocess.call("psql tournament -f tournament.sql", shell=True)
+subprocess.call("psql -f tournament.sql", shell=True)
 print "Swiss Games Setup Complete"
